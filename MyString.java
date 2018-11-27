98a2ee3a288b6ef188759c9df3c1a243566150d7
sheinamara <ashein00@stuy.edu>
2018-11-26 21:55:31 -0500

    a string is a charsequence @subSequencegit add .!

--- MyString.java
+++ MyString.java
@@ -1,10 +1,16 @@
 public class MyString implements CharSequence/*, Comparable<CharSequence>*/{
+  //store data in an array of chars
   private char[] data;
 
+  //constructor
   public MyString(CharSequence s){
     data = new char[s.length()];
+    for (int i = 0; i < s.length(); i++){
+      data[i] = s.charAt(i);
+    }
   }
 
+  // implements charAt
   public char charAt(int index){
     if (index < 0 || index > data.length - 1){
       throw new IndexOutOfBoundsException();
@@ -12,21 +18,24 @@ public class MyString implements CharSequence/*, Comparable<CharSequence>*/{
     return data[index];
   }
 
+  // implements length
   public int length(){
-    return 1;
+    return data.length;
   }
 
+  // implements subSequence
   public CharSequence subSequence(int start, int end){
-    if (start < 0 || end < 0 || end > data.length - 1 || start > data.length - 1 || end > start){
+    if (start < 0 || end < 0 || end > data.length - 1 || start > data.length - 1 || end < start){
       throw new IndexOutOfBoundsException();
     }
-    char[] whatWeWant = new char[end-start];
+    String whatWeWant = "";
     for (int i = start; i < end; i++){
       whatWeWant += data[i];
     }
     return whatWeWant;
   }
 
+  // overrides toString
   public String toString(){
     String whatWeWant = "";
     for (int i = 0; i < data.length; i++){
@@ -34,4 +43,6 @@ public class MyString implements CharSequence/*, Comparable<CharSequence>*/{
     }
     return whatWeWant;
   }
+
+
 }
